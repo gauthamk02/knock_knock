5a6c2c7775903ab605914811e3b90f746ce1c045
Gautham Krishnan <gauthamkrishnanpriya@gmail.com>
2022-06-03 23:28:35 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/knock_knock/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.knock_knock
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'constants.dart';
+import './screens/screen.dart';
+import './screens/register_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Sign In Sign Up Ui',
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme),
+        scaffoldBackgroundColor: kBackgroundColor,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: RegisterPage(),
+    );
+  }
+}
