e5e0b7ea54a668aab4e3370b40f699f7c49f4391
Anshuman <anshuman8swain@gmail.com>
2022-06-05 23:03:01 +0530

    UI updated

--- FrontEnd/lib/screens/people-screen.dart
+++ FrontEnd/lib/screens/people-screen.dart
@@ -30,7 +30,7 @@ class PeopleScreen extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.spaceAround,
                       children: [
                         Container(
-                          height: 100,
+                          height: 139,
                           width: 110,
                           // color: Colors.accents,
                           decoration: BoxDecoration(
@@ -45,7 +45,8 @@ class PeopleScreen extends StatelessWidget {
                           children: [
                             Text(
                               "$name",
-                              style: TextStyle(fontSize: 18),
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
                             ),
                             SizedBox(
                               height: 7,
@@ -63,14 +64,15 @@ class PeopleScreen extends StatelessWidget {
                       ],
                     ),
                     Container(
-                        height: MediaQuery.of(context).size.height * 0.13,
+                        height: MediaQuery.of(context).size.height * 0.11,
+                        padding: EdgeInsets.symmetric(horizontal: 20),
                         margin:
-                            EdgeInsets.symmetric(vertical: 15, horizontal: 5),
-                        child: Text("Bio: \n$description")),
+                            EdgeInsets.symmetric(vertical: 20, horizontal: 5),
+                        child: Text("About: \n$description")),
                     SizedBox(
                       height: 19,
                     ),
-                    Text("City: $location"),
+                    Text("City: $location          "),
                     SizedBox(
                       height: 6,
                     ),
@@ -94,14 +96,14 @@ class PeopleScreen extends StatelessWidget {
             child: ExpertBlock(
                 "Anshuman Swain",
                 "English, Hindi",
-                "https://images.shiksha.com/mediadata/images/articles/1583747992phpzaxKKK.jpeg",
+                "https://avatars.githubusercontent.com/u/90995338?v=4",
                 "Kochi",
                 "Expert"),
             onTap: () => checkExperts(
                 context,
-                "https://images.shiksha.com/mediadata/images/articles/1583747992phpzaxKKK.jpeg",
+                "https://avatars.githubusercontent.com/u/90995338?v=4",
                 "Anshuman Swain",
-                "2345674567",
+                "6238337639",
                 "Expert",
                 25,
                 "Playful and intellectually curious, he is constantly juggling a variety of passions, hobbies, careers, and friend groups",
@@ -118,72 +120,72 @@ class PeopleScreen extends StatelessWidget {
             onTap: () => checkExperts(
                 context,
                 "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-                "Name Singh Khan",
+                "Gaurav Sharma",
                 "2345674567",
                 "Explorer",
                 35,
                 "Playful and intellectually curious, he is constantly juggling a variety of passions, hobbies, careers, and friend groups",
                 "Hindi, engish",
-                "Delhi"),
+                "Kochi"),
           ),
           GestureDetector(
             child: ExpertBlock(
                 "Joe Doe",
-                "English, French",
+                "English, French,German",
                 "https://images.unsplash.com/photo-1592992584424-bb6c75136025?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
                 "Kochi",
                 "Explorer"),
             onTap: () => checkExperts(
                 context,
                 "https://images.unsplash.com/photo-1592992584424-bb6c75136025?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
-                "Name Singh Khan",
+                "Joe Doe",
                 "2345674567",
                 "Explorer",
                 30,
-                "descripf and sdk dfnwd f efnkejrg efb dgbjne grbbnedkjrg erkjvevn ergkn vkejv evjkevne rvn vekjrnvj rfnnervjner",
+                "Playful and intellectually curious, he is constantly juggling a variety of passions, hobbies, careers, and friend groups",
                 "Hindi, engish",
-                "Kaatmandu"),
+                "Kochi"),
           ),
           GestureDetector(
             child: ExpertBlock(
                 "Jessica H",
-                "English, Hindi",
+                "English,Malayalam",
                 "https://images.unsplash.com/photo-1518577915332-c2a19f149a75?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2062&q=80",
                 "Kochi",
                 "Expert"),
             onTap: () => checkExperts(
                 context,
                 "https://images.unsplash.com/photo-1518577915332-c2a19f149a75?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2062&q=80",
-                "Name Singh Khan",
+                "Kabir Singh Khan",
                 "2345674567",
                 "Expert",
                 34,
-                "descripf and sdk dfnwd f efnkejrg efb dgbjne grbbnedkjrg erkjvevn ergkn vkejv evjkevne rvn vekjrnvj rfnnervjner",
+                "Playful and intellectually curious, he is constantly juggling a variety of passions, hobbies, careers, and friend groups",
                 "Hindi, engish",
                 "Kochi"),
           ),
           GestureDetector(
             child: ExpertBlock(
                 "Ivan Rossi",
-                "English, Hindi",
+                "English,Malayalam",
                 "https://images.unsplash.com/photo-1618077360395-f3068be8e001?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
                 "Kochi",
                 "Explorer"),
             onTap: () => checkExperts(
                 context,
                 "https://images.unsplash.com/photo-1618077360395-f3068be8e001?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
-                "Name Singh Khan",
-                "2345674567",
+                "Ivan Rossi",
+                "2345673567",
                 "Explorer",
                 25,
-                "descripf and sdk dfnwd f efnkejrg efb dgbjne grbbnedkjrg erkjvevn ergkn vkejv evjkevne rvn vekjrnvj rfnnervjner",
+                "Playful and intellectually curious, he is constantly juggling a variety of passions, hobbies, careers, and friend groupsr",
                 "Hindi, engish",
                 "Kochi"),
           ),
           GestureDetector(
             child: ExpertBlock(
                 "Elon Musk",
-                "English, Hindi",
+                "English",
                 "https://media.gq.com/photos/566ac3fec0f741b430e641e7/master/pass/elon-musk-gq-1215-01-sq.jpg",
                 "Kochi",
                 "Expert"),
@@ -194,7 +196,7 @@ class PeopleScreen extends StatelessWidget {
                 "2345674567",
                 "Expert",
                 45,
-                "descripf and sdk dfnwd f efnkejrg efb dgbjne grbbnedkjrg erkjvevn ergkn vkejv evjkevne rvn vekjrnvj rfnnervjner",
+                "Playful and intellectually curious, he is constantly juggling a variety of passions, hobbies, careers, and friend groups",
                 "Hindi, engish",
                 "Kochi"),
           ),
@@ -208,17 +210,17 @@ class PeopleScreen extends StatelessWidget {
             onTap: () => checkExperts(
                 context,
                 "https://global-uploads.webflow.com/5dfd5aca7badfa129f80056c/5ed6d2e60331fd5be59cef4c_5ed5901b8c8b1f3f205e088e_63O1znGw_400x400.jpeg",
-                "Name Singh Khan",
+                "Tim Cook",
                 "2345674567",
                 "Explorer",
                 29,
-                "descripf and sdk dfnwd f efnkejrg efb dgbjne grbbnedkjrg erkjvevn ergkn vkejv evjkevne rvn vekjrnvj rfnnervjner",
+                "Playful and intellectually curious, he is constantly juggling a variety of passions, hobbies, careers, and friend groups",
                 "Hindi, engish",
                 "Kochi"),
           ),
           ExpertBlock(
               "Larry Page",
-              "English, Hindi",
+              "English, French",
               "https://upload.wikimedia.org/wikipedia/commons/e/ec/Larry_Page_in_the_European_Parliament%2C_17.06.2009_%28cropped%29.jpg",
               "Kochi",
               "Explorer"),
